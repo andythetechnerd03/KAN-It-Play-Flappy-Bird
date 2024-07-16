ac686a303bd757db43eed34c4d1d6b1f98146fa0
Dinh Ngoc An <dinhngocan102003@gmail.com>
2024-07-16 19:19:06 +0700

    train kan 16

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Run the agent")
     parser.add_argument("--env", type=str, default="cartpole", help="environment to run the agent")
     parser.add_argument("--train", action="store_true", help="train the agent")
+    parser.add_argument("--render", action="store_true", help="whether to render the environment (a.k.a. display the gameplay),\
+                        use it for testing only.")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -25,4 +27,4 @@ if __name__ == "__main__":
     if args.train:
         agent.run(train=args.train, render=False)
     else:
-        agent.test(render=True, num_episodes=10)
+        agent.test(render=True, num_episodes=10, render=args.render)
